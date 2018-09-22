dd10addb12ee692b6cfc571df855277dea8a88f9
tosha4encko <toshapan4encko@yandex.ru>
2018-09-22 19:39:35 +0300

    Убрал мусор

--- my_TB/DataType/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Data, Type
-
-admin.site.register(Data)
-admin.site.register(Type)
--- my_TB/my_TB/settings.py
+++ my_TB/my_TB/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'TB',
-    'main',
-    'DataType',
+    'main'
 ]
 
 MIDDLEWARE = [
